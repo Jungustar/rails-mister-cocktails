3bef03a6f4be2cc48c29575a7dacf0c3062f9187
Juergen Nguyen <juergen.nguyen@gmail.com>
2019-08-16 22:02:33 +0200

    add code

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
     root to: 'cocktails#index'
-    resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
       resources :doses, only: [:create, :new]
+      resources :reviews, only: [:new, :create]
   end
 end
