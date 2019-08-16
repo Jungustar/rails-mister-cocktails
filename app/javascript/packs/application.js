39cbd470462e23594d25e7427babbe6630562c6b
Juergen Nguyen <juergen.nguyen@gmail.com>
2019-08-16 12:23:39 +0200

    add code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,5 @@
 import 'bootstrap';
 import { updateNavbarOnScroll } from '../components/navbar';
-import { dynamicBannerText } from '../components/banner';
 
 console.log('Hello World from Webpacker')
 updateNavbarOnScroll();
-dynamicBannerText();
