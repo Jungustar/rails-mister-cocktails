1407271b54ac0a5f54fde7f66dc9427fb467d6a8
Juergen Nguyen <juergen.nguyen@gmail.com>
2019-08-16 16:23:02 +0200

    add code

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,9 +2,7 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $(document).ready(function() {
-    $('.select-dropdown').select2();
-  });
+  $('.select2').select2(); // (~ document.querySelectorAll)
 };
 
 export { initSelect2 };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
+import 'select2/dist/css/select2.css'
 import { updateNavbarOnScroll } from '../components/navbar';
-import {initSelect2} from '../components/navbar'
+import {initSelect2} from '../components/init_select2'
 
 console.log('Hello World from Webpacker')
 updateNavbarOnScroll();
